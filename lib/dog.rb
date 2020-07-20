a57c45d8a517764c173b6aa0fc1cd10ee119bf9f
Ebo Lee <yiboli.coding@gmail.com>
2020-07-20 21:52:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  def name=("name")
+  def name=(name)
     @name = "name"
   end 
   
@@ -10,3 +10,22 @@ class Dog
   def bark
     puts "woof!"
   end 
+
+
+
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
+ 
